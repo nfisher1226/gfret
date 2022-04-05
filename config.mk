21633a4b36d1f5a9fc71beffd570d23cdfed1034
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-04-05 12:15:43 -0400

    Removed Makefile, now uses cargo for complete build

--- config.mk
+++
@@ -1,8 +0,0 @@
-GUIPROG     ?= gfret
-PREFIX      ?= /usr/local
-BINDIR      ?= $(DESTDIR)$(PREFIX)/bin
-DATADIR     ?= $(DESTDIR)$(PREFIX)/share
-XDGDIR      ?= $(DATADIR)/applications
-MANDIR      ?= $(DATADIR)/share/man
-ICONDIR     ?= $(DATADIR)/icons/hicolor
-PNGICONS    ?= false

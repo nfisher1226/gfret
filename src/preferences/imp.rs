0d951466e96f52ae23af00ca8f5b517a20f5ab20
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-08-15 11:46:31 -0400

    Clean up preferences ui file and imp module

--- src/preferences/imp.rs
+++ src/preferences/imp.rs
@@ -25,33 +25,19 @@ pub struct PreferencesWindow {
     #[template_child]
     pub external_button: TemplateChild<gtk::Button>,
     #[template_child]
-    external_row: TemplateChild<adw::EntryRow>,
-    #[template_child]
-    border_row: TemplateChild<adw::ActionRow>,
-    #[template_child]
     pub border_width: TemplateChild<gtk::SpinButton>,
-    #[template_child]
-    line_weight_row: TemplateChild<adw::ActionRow>,
     #[template_child]
     pub line_weight: TemplateChild<gtk::SpinButton>,
     #[template_child]
-    fretline_color_row: TemplateChild<adw::ActionRow>,
-    #[template_child]
     fretline_color: TemplateChild<gtk::ColorButton>,
     #[template_child]
-    fretboard_color_row: TemplateChild<adw::ActionRow>,
-    #[template_child]
     pub fretboard_color: TemplateChild<gtk::ColorButton>,
-    #[template_child]
-    centerline_row: TemplateChild<adw::ActionRow>,
     #[template_child]
     pub draw_centerline: TemplateChild<gtk::Switch>,
     #[template_child]
     centerline_color_row: TemplateChild<adw::ActionRow>,
     #[template_child]
     pub centerline_color: TemplateChild<gtk::ColorButton>,
-    #[template_child]
-    print_row: TemplateChild<adw::ActionRow>,
     #[template_child]
     print_specs: TemplateChild<gtk::Switch>,
     #[template_child]
@@ -79,15 +65,6 @@ impl ObjectSubclass for PreferencesWindow {
 impl ObjectImpl for PreferencesWindow {
     fn constructed(&self, obj: &Self::Type) {
         self.parent_constructed(obj);
-        self.external_row.get().add_suffix(&self.external_button.get());
-        self.border_row.get().add_suffix(&self.border_width.get());
-        self.line_weight_row.get().add_suffix(&self.line_weight.get());
-        self.fretline_color_row.get().add_suffix(&self.fretline_color.get());
-        self.fretboard_color_row.get().add_suffix(&self.fretboard_color.get());
-        self.centerline_row.get().add_suffix(&self.draw_centerline.get());
-        self.centerline_color_row.get().add_suffix(&self.centerline_color.get());
-        self.print_row.get().add_suffix(&self.print_specs.get());
-        self.font_row.get().add_suffix(&self.font_chooser.get());
         // Bind some properties
         self.draw_centerline.get()
             .bind_property("active", &self.centerline_color_row.get(), "visible")

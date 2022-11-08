e49b826e39444ff6646b9b5999c9bc5d3acad78a
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-11-08 10:38:48 -0500

    Formatted, fixed `deny.toml`

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -7,59 +7,38 @@ use {
 };
 
 /// Runs the cli
+/// # Panics
+/// Because all of the values retrieved using `unwrap()` have a default value
+/// set, this function should never actually panic.
 pub fn run(matches: &ArgMatches) {
-    let scale: f64 = match matches.get_one::<f64>("SCALE") {
-        Some(c) => *c,
-        None => {
-            process::exit(1);
-        }
-    };
-    let count = match matches.get_one("COUNT") {
-        Some(c) => *c,
-        None => {
-            process::exit(1);
-        }
-    };
-    let variant = if let Some(c) = matches.get_one("MULTI") {
-        let scale = *c;
+    let scale = *matches.get_one("SCALE").unwrap();
+    let count = *matches.get_one("COUNT").unwrap();
+    let variant = if let Some(s) = matches.get_one("MULTI") {
         let handedness = if matches.get_flag("LEFT") {
             Handedness::Left
         } else {
             Handedness::Right
         };
-        let pfret = match matches.get_one("PERPENDICULAR") {
-            Some(c) => *c,
-            None => {
-                process::exit(1);
-            }
-        };
+        let pfret = *matches.get_one("PERPENDICULAR").unwrap();
         Variant::Multiscale {
-            scale,
+            scale: *s,
             handedness,
             pfret,
         }
     } else {
         Variant::Monoscale
     };
-    let nut = match matches.get_one("NUT") {
-        Some(c) => *c,
-        None => {
-            process::exit(1);
-        }
-    };
-    let bridge: f64 = match matches.get_one("BRIDGE") {
-        Some(c) => c + 6.0,
-        None => {
-            process::exit(1);
-        }
-    };
+    let nut = *matches.get_one("NUT").unwrap();
+    let bridge = matches.get_one("BRIDGE").unwrap() + 6.0;
     let specs = Specs::init(scale, count, variant, nut, bridge);
     let doc = specs.create_document(None);
-    let output = matches.get_one::<String>("OUTPUT").map(|x| x.to_string()).unwrap_or("-".to_string());
+    let output = matches
+        .get_one::<String>("OUTPUT")
+        .map_or("-", std::string::String::as_str);
     if output == "-" {
         println!("{doc}");
     } else {
-        match svg::save(&output, &doc) {
+        match svg::save(output, &doc) {
             Ok(_) => println!("Output saved as {output}."),
             Err(e) => {
                 eprintln!("{e}");
--- src/cli/opts.rs
+++ src/cli/opts.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, ArgAction, Command, value_parser};
+use clap::{value_parser, Arg, ArgAction, Command};
 
 /// The cli subcommand options
 #[must_use]
@@ -12,67 +12,51 @@ known in advance.",
         )
         .author("The JeanG3nie <jeang3nie@hitchhiker-linux.org>")
         .version(env!("CARGO_PKG_VERSION"))
-        .arg(
+        .args([
             Arg::new("SCALE")
                 .help("Scale length in mm")
                 .short('s')
                 .long("scale")
                 .value_parser(value_parser!(f64))
                 .default_value("648"),
-        )
-        .arg(
             Arg::new("MULTI")
                 .help("Multiscale fretboard with <MULTI> as the treble scale")
                 .short('m')
                 .long("multi")
                 .value_parser(value_parser!(f64))
                 .default_value("610"),
-        )
-        .arg(
             Arg::new("LEFT")
                 .help("Multiscale fretboard reversed (left handed)")
                 .short('l')
                 .long("left")
                 .action(ArgAction::SetTrue),
-        )
-        .arg(
             Arg::new("PERPENDICULAR")
                 .help("Which fret is perpendicular to the centerline")
                 .short('p')
                 .long("perpendicular")
                 .value_parser(value_parser!(f64))
                 .default_value("8"),
-        )
-        .arg(
             Arg::new("COUNT")
                 .help("Total fret count")
                 .short('c')
                 .long("count")
                 .value_parser(value_parser!(u32))
                 .default_value("24"),
-        )
-        .arg(
             Arg::new("NUT")
                 .help("Nut width")
                 .short('n')
                 .long("nut")
                 .value_parser(value_parser!(f64))
                 .default_value("43"),
-        )
-        .arg(
             Arg::new("BRIDGE")
                 .help("Bridge Spacing")
                 .short('b')
                 .long("bridge")
                 .value_parser(value_parser!(f64))
                 .default_value("56"),
-        )
-        .arg(
             Arg::new("OUTPUT")
                 .help("Name of the output file")
                 .default_value("output.svg"),
-        )
-        .arg(
             Arg::new("EXTERN")
                 .help("Open output file in external program")
                 .short('e')
@@ -80,7 +64,7 @@ known in advance.",
                 .num_args(0..=1)
                 .value_parser(value_parser!(String))
                 .default_missing_value("inkscape"),
-        )
+        ])
 }
 
 /// The main program options
